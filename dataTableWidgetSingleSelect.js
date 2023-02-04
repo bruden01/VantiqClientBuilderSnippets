7cb2c6dbfd58b2f69a0da6a5efd98988c558a7e0
bruden01 <brett.rudenstein@gmail.com>
2023-02-04 14:04:32 -0500

    Create dataTableWidgetSingleSelect.js

---
+++ dataTableWidgetSingleSelect.js
@@ -0,0 +1,12 @@
+// when checked uncheck all other rows
+var oneChecked = false;
+$('#DataTableCommonName :checkbox').each(function() {
+  if (extra.dataRowIndex !== $(this).data("row")){
+    this.checked = false;
+  } 
+  // checked to see if at least one is checked
+  if (this.checked){
+    oneChecked = true;
+    page.data.selected = extra.dataObject;
+  } 
+});
